1de4526b08e8729742f35661e7af7c3a7039f7b9
hannahvw <103385894+hannahvw@users.noreply.github.com>
2024-04-20 13:52:08 -0500

    Added email validation

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const submitBtn = document.querySelector("#submit-btn");
 const userInput = document.querySelector("#email");
+const errorIcon = document.querySelector(".error-icon");
+const errorMsg = document.querySelector(".error-msg")
+
+function signUp(event) {
+if (!userInput.value.includes("@")) {
+// errorMsg.removeAttribute("class", "hidden");
+//errorIcon.removeAttribute("class", "hidden");
+errorIcon.style.display = "block";
+errorMsg.style.display = "block";
+} 
+}
+
+submitBtn.addEventListener("click", signUp);
