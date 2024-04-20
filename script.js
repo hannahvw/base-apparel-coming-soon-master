bb1f536f5fe141c21fc3c269364bd7e47df39524
hannahvw <103385894+hannahvw@users.noreply.github.com>
2024-04-20 17:37:29 -0500

    Fixed error message

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const submitBtn = document.querySelector("#submit-btn");
 const userInput = document.querySelector("#email");
 const errorIcon = document.querySelector(".error-icon");
-const errorMsg = document.querySelector(".error-msg")
+const errorMsg = document.querySelector(".error-msg");
 
 function signUp(event) {
-if (!userInput.value.includes("@")) {
+  if (!userInput.value.includes("@")) {
     event.preventDefault();
-// errorMsg.removeAttribute("class", "hidden");
-//errorIcon.removeAttribute("class", "hidden");
-errorIcon.style.display = "block";
-errorMsg.style.display = "block";
-} 
+    errorMsg.removeAttribute("class", "hidden");
+    errorIcon.removeAttribute("class", "hidden");
+    errorMsg.setAttribute("class", "error-msg");
+    errorIcon.setAttribute("class", "error-icon");
+    //errorIcon.style.display = "block";
+    //errorMsg.style.display = "block";
+  }
 }
 
 submitBtn.addEventListener("click", signUp);
